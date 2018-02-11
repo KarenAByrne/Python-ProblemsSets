28549882a345a35be78a46cdc685fc60e2a1d011
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-02-11 21:04:24 +0000

    Update fib.py
     Test the function with the following value. My name is Karen , so the first and last letter of my name (K+ N = 11 + 14) give the number 25. The 25th Fibonacci number is 75025.
    Fibonacci number 25 is 75025
    I had some issues. I followed the helpful thread so thanks to all contributors. 
    
    
    My surname is Byrne
    
    The first letter B is number 66
    
    The last letter e is number 101
    
    Fibonacci number 167 is 35600075545958458963222876581316753
    
    Ord: Given a string of length one, return an integer representing the Unicode code point of the character when the argument is a unicode object, or the value of the byte when the argument is an 8-bit string.

--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-# Ian McLoughlin
+# Karen Byrne
 # A program that displays Fibonacci numbers.
 
 def fib(n):
@@ -13,7 +13,38 @@ def fib(n):
   
   return i
 
-# Test the function with the following value.
+# Test the function with the following value. My name is Karen , so the first and last letter of my name (K+ N = 11 + 14) 
+# give the number 25. The 25th Fibonacci number is 75025.
+# Fibonacci number 25 is 75025
+# I had some issues. I followed the helpful thread so thanks to all contributors. 
 x = 25
 ans = fib(x)
+print("Fibonacci number", x, "is", ans)
+
+# Karen Byrne
+# A program that displays Fibonacci numbers using people's names.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Byrne"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
