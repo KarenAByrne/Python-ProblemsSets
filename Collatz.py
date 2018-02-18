82de1edeeeff505ab7b3621c42256383dcdaf309
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-02-18 21:54:05 +0000

    Added comments

--- Collatz.py
+++ Collatz.py
@@ -1,14 +1,19 @@
-#Karen Byrne Week 3 Exercise
+#Karen Byrne 18-02-2018
+#Week 3 Exercise
 
 i=17
 
-while (i>1):
-    if (i%2==0):
+while (i>1):        #loop down to 1
+    if (i%2==0):    #number is even
         i=i%2
         print(i)
-        else:
+        else:       #number is odd
         i=3*i+1
         print(i)
+    
+
+ 
+
     
 
  
