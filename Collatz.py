504419bd70165467df82698b9f479df22da052c9
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-02-18 21:56:49 +0000

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -7,7 +7,7 @@ while (i>1):        #loop down to 1
     if (i%2==0):    #number is even
         i=i%2
         print(i)
-        else:       #number is odd
+    else:            #number is odd
         i=3*i+1
         print(i)
     
