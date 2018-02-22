ae8800ae2e0e6b5f5c78c22eabd126e9c47043a1
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-02-22 14:13:25 +0000

    Update Collatz.py
    Incorrect function used for divide. corrected and tidied up. Need to work on getting rid of floating point

--- Collatz.py
+++ Collatz.py
@@ -1,15 +1,16 @@
 #Karen Byrne 18-02-2018
 #Week 3 Exercise
+# 22-02-2018
 
-i=17
+i = 17
 
-while (i>1):        #loop down to 1
-    if (i%2==0):    #number is even
-        i=i%2
-        print(i)
-    else:            #number is odd
-        i=3*i+1
-        print(i)
+
+while (i > 1):        #loop down to 1
+    if (i%2 == 0):    #number is even
+        i = i / 2
+    else:       #number is odd
+        i = 3 * i + 1
+    print(i)
     
 
  
