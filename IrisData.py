99a8ac3c334939cdcb8ad29d18545c05ed7e9133
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-03-29 14:58:18 +0100

    Update IrisData.py
    Code that works with added commentary

--- IrisData.py
+++ IrisData.py
@@ -1,7 +1,9 @@
 #Karen Byrne 29/03/2018
 #Week 5 Iris data recall in neat format
+# https://learnonline.gmit.ie/mod/forum/discuss.php?d=14986
 
 with open("data/iris.csv") as f:
     for line in f:
-        print(line.split(',')[:4]) # From discussion post https://learnonline.gmit.ie/mod/forum/discuss.php?d=14986
-
+       line = line.replace(',',' ') #Replaces commas from the line with a space
+       print(line[:11],line[12:16].strip()) #Prints the first 12 (0-11)  characters with a 
+       #space instead of a comma, strips 16 characters from the 12th char onwards
