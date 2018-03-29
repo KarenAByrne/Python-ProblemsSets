683e97d08f224fd2c6745485213fe57786dd6d3c
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-03-29 13:04:53 +0100

    Update IrisData.py
    Attempt to read data line by line

--- IrisData.py
+++ IrisData.py
@@ -1,10 +1,7 @@
 #Karen Byrne 29/03/2018
 #Week 5 Iris data recall in neat format
-f = open("data/iris.csv",'r') # open the file to read only
 
-print (f.read()) # print out the contents of the file
-
-f.close() # clsoe the file
-
-
+with open("data/iris.csv",'r') as f
+    for line in f:
+        print(line,end'')
 
