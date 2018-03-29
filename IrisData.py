07be29a3419ab2a075d74cd538671e6f329aab8f
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-03-29 12:52:29 +0100

    Update IrisData.py
    Add comments

--- IrisData.py
+++ IrisData.py
@@ -1,10 +1,10 @@
 #Karen Byrne 29/03/2018
 #Week 5 Iris data recall in neat format
-f = open("data/iris.csv",'r')
+f = open("data/iris.csv",'r') # open the file to read only
 
-print (f.read())
+print (f.read()) # print out the contents of the file
 
-f.close()
+f.close() # clsoe the file
 
 
 
