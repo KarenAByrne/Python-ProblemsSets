3febdcd46691eeaebf676420c1b0f0d08cb7100d
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-03-29 19:57:51 +0100

    Update Factorial.py
    Working code for Problem 6, all my own work following the function lecture.

--- Factorial.py
+++ Factorial.py
@@ -2,12 +2,12 @@
 #Exercise 6 Factorial
 
 def factorial(input):
-    input = 5
-    tempfactx=1
-    while input>0:
-        tempfactx = tempfactx * (input)
-        input=input-1
-    return tempfactx
+    tempfactx=1 #need to multiply input by 1 to start the loop
+    while input>0: # while loop down to 1 as per factorial calculation
+        tempfactx = tempfactx * (input) # first number is 1*5
+        input=input-1 # reduce input by 1 each loop and multiply by previous tempfactx
+    return tempfactx # output next factorial in series to feed back into the loop
     
-
 print("The factorial of 5 is",factorial(5))
+print("The factorial of 7 is",factorial(7))
+print("The factorial of 10 is",factorial(10))
