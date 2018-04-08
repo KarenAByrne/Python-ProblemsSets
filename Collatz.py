c97191658a388f3908a9175ee23af3916101ea05
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-04-08 16:48:02 +0100

    Update Collatz.py
    Added more comments

--- Collatz.py
+++ Collatz.py
@@ -1,14 +1,14 @@
-#Karen Byrne 18-02-2018
-#Week 3 Exercise
+# Karen Byrne 18-02-2018
+# Week 3 Exercise
 # 22-02-2018
 
 i = 17
 
 
-while (i > 1):        #loop down to 1
-    if (i%2 == 0):    #number is even
-        i = i / 2
+while (i > 1):        # loop down to 1
+    if (i%2 == 0):    # if the number is even
+        i = i / 2       # divide by 2 if even
     else:       #number is odd
-        i = 3 * i + 1
-    print(int(i)) # return an integer for i
+        i = 3 * i + 1 # multiply by 3 and add 1 if odd
+    print(int(i)) # return an integer for i, prints each number and stops when reaches 1
    
