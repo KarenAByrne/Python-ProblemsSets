f1739ebdc61b9aab08f66597b92008004d593d92
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-04-08 16:50:59 +0100

    Update IrisData.py
    Comments added

--- IrisData.py
+++ IrisData.py
@@ -1,6 +1,9 @@
-#Karen Byrne 29/03/2018
-#Week 5 Iris data recall in neat format
+# Karen Byrne 29/03/2018
+# Week 5 Iris data recall in neat format
 # https://learnonline.gmit.ie/mod/forum/discuss.php?d=14986
+# I dont think this returns and integer only a string. I will need to 
+# work on this for the bigger Project. 
+# Below shows the data being opened and closed and contents returned.
 
 with open("data/iris.csv") as f:
     for line in f:
