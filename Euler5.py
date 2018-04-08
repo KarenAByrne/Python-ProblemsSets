12e182efce96e28117e0141cdcd3a534fc6bd821
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-04-08 11:29:19 +0100

    Update Euler5.py
    Some output

--- Euler5.py
+++ Euler5.py
@@ -1,12 +1,11 @@
 #Karen Byrne 8th April 2018
-#Euler 5
-
-SPI = 2520
+#Euler 5)
+Num = 2520
+SPI = True
 # Define Smallest positive integer
-for i in range(1,20):
-        if SPI % i == 0:
+for i in range(11,20):
+        if Num % i == 0:
             SPI = True
         else:
-            SPI = SPI + 2
-return SPI
-
+            Num = Num + 2
+print(Num)
